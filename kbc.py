618a943cb51419b93030a8ffb17bd46778f65b87
Imemyslf <21co51@aitdgoa.edu.in>
2023-09-21 23:51:50 +0530

    addedques

--- kbc.py
+++ kbc.py
@@ -5,11 +5,14 @@ ques = [
         ["Fastets animal in the world?","Tiger","Girrafe","Chetah","Lion",3],
         ["Longest river in the world?","Amazon","Nile","Godavri","Kaveri",2],
         ["Heighest Architecture Building in the world?","Sydney Opera House","Eiffel Tower","Leaning Tower of Pis","Burj Khalifa",4],
+        ["Tallest human made Statue in the world?","Spring Temple Buddha"," Laykyun Sekkya","Statue of Unity","Statue of Belief",3]
         ["Which of the following is a root vegetable?","Carrot","Cassava","Onion","All the above ",4],
         ["Which of the following diseases is caused by the deficiency of Vitamin C?","Scurvy","Chronic Inflammation and Oxidative Stress","Bleeding Gums","All the above",4],
+        ["Which animal is Yama's vehicle?","Crocodile","Buffalo","Tiger","None of the above",2],
         ["In which of the following state of India Valmiki Tiger Reserve is located?","Chattisgarh","Jharkhand","Bihar","Madhya Pradesh",3],
         ["Full of RAC related to Railways are?","Retail Asset Centre","Reservation Against Cancellation","Rajasthan Armed Constabulary","Refregeration & Air Conditioning",2],
         ["The first non-congress PM who completed a full term as PM in India?","Atal Bihari Vajpayee","H. D. Deve Gowda","Narendra Modi","Inder Kumar Gujra",1],
+        ["Whic one of thr first avatar of Lord Vishnu from his Dashavatar?","Matsya","Kurma","Varah","None of the above",1],
 ]
 
 monpri = [1000,2000,3000,5000,10000,20000,40000,80000,160000,320000,640000,1250000,2500000,5000000,7500000,10000000]
